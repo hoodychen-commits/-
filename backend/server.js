cfdef68765f978c1bf04818f42d43b26c4a6f124
hoodychen <hoodychen@gmail.com>
2026-04-16 20:21:41 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -279,7 +279,7 @@ app.get('/api/admin/products', authenticateToken, requireAdmin, (req, res) => {
 // Serve Static React Frontend
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
 });
 
